61bfa794c02327b1b4508d40be5ec5a2e62d3722
Alex Loftus <alexloftus2004@gmail.com>
2018-11-20 19:03:25 -0500

    first commit

---
+++ gen-funcs.py
@@ -0,0 +1,12 @@
+""" 
+Dance moves:
+    - right-side pass
+    - left-side pass
+      - inside-turn
+    - sugar-push
+    - sugar-tuck
+    - whip
+      - outside-turn
+      - inside-turn 
+"""
+
---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Dance moves generator. Run this to generate random dance moves.
+Goals for this package:
+  - Have it use multiple modules
+  - Be able to run from the command line with flags
+    - Flags : -h, -randomize
+  - Use virtual environment
+  - Dance moves stored in separate file
+ """
+
+
+
+MOVES = ['right-side pass', 'left-side pass', 'sugar-push', 'sugar-tuck', 'whip', 'inside-turn', 'outside-turn']
