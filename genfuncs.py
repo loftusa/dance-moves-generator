61564a8c84690e251d2d3d6caf7095e082f7921d
Alex Loftus <alexloftus2004@gmail.com>
2018-11-22 15:54:24 -0500

    It's working but the code is structured like crap

--- genfuncs.py
+++ genfuncs.py
@@ -11,8 +11,13 @@ Dance moves:
 """
 
 import random
+from itertools import combinations
 
 def random_move(mvs) -> str:
     """ Return a random element of mvs, pop it out of mvs. """
     random.shuffle(mvs)
     return mvs.pop()
+
+def random_comb(mvs):
+	""" Return a random combination of two moves """
+	return list(combinations(mvs, 2))
--- main.py
+++ main.py
@@ -3,23 +3,51 @@ Dance moves generator. Run this to generate random dance moves.
 Goals for this package:
   - Have it use multiple modules
   - Be able to run from the command line with flags
-    - Flags : [-h] [-m [MOVE]]
+	- Flags : [-h] [-m [MOVE]]
   - Use virtual environment
   - Dance moves stored in separate file
   - It's gonna be kinda overengineered but that's ok
  """
 
-import genfuncs
 import random
+from itertools import combinations
+
+import genfuncs
 
 MOVES = ['right-side pass', 'left-side pass', 'sugar-push',
-         'sugar tuck', 'whip', 'inside turn', 'outside turn',]
+		 'sugar tuck', 'whip', 'inside turn', 'outside turn', ]
+
+OPERATORS = ['Rock and Go!']
 
 
 def main():
-    first = genfuncs.random_move(MOVES)
-    second = genfuncs.random_move(MOVES)
-    print('Combine {0} and {1}'.format(first, second))
+	print("\nm : random move\n" + "c : random two moves\n" + "q : quit")
+	mv = MOVES.copy()
+	mvs = genfuncs.random_comb(MOVES.copy())
 
-if __name__ == "__main__":
-    main()
+	while True:
+
+		f = input()
+		
+		if f == 'm':
+			op = random.choice([0, 1])
+			print(mv.pop())
+			if op:
+				print(random.choice(OPERATORS))
+			if not mv:
+				mv = MOVES.copy()
+
+
+		elif f == 'c':
+			op = random.choice([0, 1])
+			print(mvs.pop())
+			if op:
+				print(random.choice(OPERATORS))
+			if not mvs:
+				mvs = genfuncs.random_comb(MOVES.copy())
+
+		elif f == 'q':
+			break
+
+if __name__ == '__main__':
+	main()
